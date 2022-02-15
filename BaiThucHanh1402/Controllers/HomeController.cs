af39d918ce6d9970993ddab4a48a4d5bd2b04d0a
Nguyen Van Cuong <1521050560@student.humg.edu.vn>
2022-02-15 23:34:23 +0700

    Bai Thuc hanh 14/02

--- BaiThucHanh1402/Controllers/HomeController.cs
+++ BaiThucHanh1402/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using BaiThucHanh1402.Models;
 
+
 namespace BaiThucHanh1402.Controllers
 {
     public class HomeController : Controller
@@ -17,11 +18,62 @@ namespace BaiThucHanh1402.Controllers
         {
             _logger = logger;
         }
-
+        GiaiPT gpt = new GiaiPT();
+        GiaiPTB2 gptb2 = new GiaiPTB2();
         public IActionResult Index()
         {
             return View();
         }
+
+        
+        public ActionResult GiaiPTBac1()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GiaiPTBac1(double hesoa, double hesob)
+        {
+            double x = gpt.GiaiPTBac1(hesoa, hesob);
+            ViewBag.nghiemPT = x;
+            return View();
+        }
+
+        public ActionResult GiaiPTBac2()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GiaiPTBac2(double hesoa, double hesob, double hesoc)
+        {
+            double x1,x2;
+            double delta;
+            delta= gptb2.GiaiPTBac2(hesoa, hesob, hesoc);
+            if(delta < 0)
+            { 
+                ViewBag.KetLuan = "PT Vo Nghiem!";
+            }
+            else if(delta==0)
+            {                
+                ViewBag.KetLuan = "PT Co Nghiem la: ";
+                x1 = x2 = -hesob/(2*hesoa);    
+                ViewBag.nghiemPTx1 = x1;
+                ViewBag.nghiemPTx2 = x2;           
+            }
+            else
+            {
+                ViewBag.KetLuan = "PT Co Nghiem la: ";
+                delta = Math.Sqrt(delta);
+                x1 = (-hesob + delta) / (2*hesoa);
+                x2 = (-hesob - delta) / (2*hesoa);
+                ViewBag.nghiemPTx1 = x1;
+                ViewBag.nghiemPTx2 = x2;
+            }
+            return View();
+        }
+
+
 
         public IActionResult Privacy()
         {
